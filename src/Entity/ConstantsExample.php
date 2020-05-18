d1d3b83e83e6e822b7bc85506a116c850ba4ec7e
Pawel RADYNA <radyna.pawel@gmail.com>
2020-05-18 19:32:08 +0200

    add ConstantsExample

---
+++ src/Entity/ConstantsExample.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity;
+
+
+class ConstantsExample
+{
+    public const PRODUCT_PICTURES_DIRECTORY = '/public/uploads/product_pictures';
+    public const PRODUCT_PICTURES_DIRECTORY_TWIG = 'uploads/product_pictures/';
+    public const PROFILE_PICTURES_DIRECTORY = '/public/uploads/profile_pictures';
+    public const EMAIL = ['gamesmarket@gmail.com' => 'Games Market'];
+    public const PAYPAL_CLIENT_ID = '';
+}
